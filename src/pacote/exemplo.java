dfdd6ab3c79cf106319aed50fed567649673e996
DannyCastroVentura <dannycastroventura@gmail.com>
2020-11-15 12:50:43 +0000

    Lab 02 online

--- src/pacote/exemplo.java
+++ src/pacote/exemplo.java
@@ -1,38 +1,27 @@
-// Java program to find IP address of your computer 
-// java.net.InetAddress class provides method to get 
-// IP of any host name
 package pacote;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.InetAddress;
-import java.net.URL;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Enumeration;
 
-public class exemplo
-{
-    public static void main(String args[]) throws Exception
-    {
-        // Returns the instance of InetAddress containing
-        // local host name and address
-        InetAddress localhost = InetAddress.getLocalHost();
-        System.out.println("System IP Address : " +
-                (localhost.getHostAddress()).trim());
+import static java.lang.System.out;
 
-        // Find public IP address
-        String systemipaddress = "";
-        try
-        {
-            URL url_name = new URL("http://bot.whatismyipaddress.com");
+public class exemplo {
 
-            BufferedReader sc =
-                    new BufferedReader(new InputStreamReader(url_name.openStream()));
-
-            // reads system IPAddress
-            systemipaddress = sc.readLine().trim();
-        }
-        catch (Exception e)
-        {
-            systemipaddress = "Cannot Execute Properly";
-        }
-        System.out.println("Public IP Address: " + systemipaddress +"\n");
+    public static void main(String args[]) throws SocketException {
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets))
+            displayInterfaceInformation(netint);
     }
-} 
+
+    static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+        out.printf("Display name: %s\n", netint.getDisplayName());
+        out.printf("Name: %s\n", netint.getName());
+        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+            out.printf("InetAddress: %s\n", inetAddress);
+        }
+        out.printf("\n");
+    }
+}
